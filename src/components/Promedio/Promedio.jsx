b79edaeea6f80bb4dc0585ec703bc3f8c475208d
AncizarTorres <ancizar_19_99@hotmail.com>
2024-05-03 17:08:11 -0500

    Refactor Promedio component to handle null values in notaCorte variables

--- src/components/Promedio/Promedio.jsx
+++ src/components/Promedio/Promedio.jsx
@@ -5,12 +5,16 @@ import LogoSecond from '../../assets/Logo-Soy-Autonoma-Soy-Aunar.png'
 
 function NotaFinalSemestre() {
 
-  const [notaCorte1, setNotaCorte1] = useState(0);
-  const [notaCorte2, setNotaCorte2] = useState(0);
-  const [notaCorte3, setNotaCorte3] = useState(0);
-  const [notaFinal, setNotaFinal] = useState(0);
+  const [notaCorte1, setNotaCorte1] = useState(null);
+  const [notaCorte2, setNotaCorte2] = useState(null);
+  const [notaCorte3, setNotaCorte3] = useState(null);
+  const [notaFinal, setNotaFinal] = useState(null);
 
   const calcularNotaFinal = () => {
+    if (!notaCorte1 || !notaCorte2 || !notaCorte3) {
+      alert('Por favor ingrese las tres notas de corte');
+      return;
+    }
     const notaFinal = ((notaCorte1 * 0.3) + (notaCorte2 * 0.3) + (notaCorte3 * 0.4)).toFixed(1);
     setNotaFinal(notaFinal);
   };
@@ -20,19 +24,39 @@ function NotaFinalSemestre() {
       <img src={LogoFirst} className="logo first" alt="LogoFirst" />
       <img src={LogoSecond} className="logo" alt="LogoSecond" />
       <h1>Calculadora de Nota Final del Semestre</h1>
-      <label>Nota Primer Corte:</label>
-      <input type="number" value={notaCorte1} onChange={(e) => setNotaCorte1(parseFloat(e.target.value))} />
+      <label htmlFor="notaCorte1">Nota Primer Corte:</label>
+      <input
+        onChange={(e) => setNotaCorte1(parseFloat(e.target.value))}
+        placeholder='Ingrese la nota del primer corte'
+        type="number"
+        value={notaCorte1}
+      />
       <br />
-      <label>Nota Segundo Corte: </label>
-      <input type="number" value={notaCorte2} onChange={(e) => setNotaCorte2(parseFloat(e.target.value))} />
+      <label htmlFor="notaCorte2">Nota Segundo Corte: </label>
+      <input
+        onChange={(e) => setNotaCorte2(parseFloat(e.target.value))}
+        placeholder='Ingrese la nota del segundo corte'
+        type="number"
+        value={notaCorte2}
+      />
       <br />
-      <label>Nota Tercer Corte: </label>
-      <input type="number" value={notaCorte3} onChange={(e) => setNotaCorte3(parseFloat(e.target.value))} />
+      <label htmlFor="notaCorte3">Nota Tercer Corte: </label>
+      <input
+        placeholder='Ingrese la nota del tercer corte'
+        type="number"
+        value={notaCorte3}
+        onChange={(e) => setNotaCorte3(parseFloat(e.target.value))}
+      />
       <br />
       <button onClick={calcularNotaFinal}>Calcular Nota Final</button>
       <br />
-      <label> Nota Final del Semestre:</label>
-      <input type="text" value={notaFinal} disabled />
+      <label htmlFor="notaFinal"> Nota Final del Semestre:</label>
+      <input
+        disabled
+        placeholder='Nota final del semestre'
+        type="text"
+        value={notaFinal}
+      />
     </>
   );
 }
